09fcb684fe63271f2059f420148f7468bcaf0d6d
root <root@vmi724319.contaboserver.net>
2021-11-24 21:47:25 +0100

    Update

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,8 +9,12 @@ require("@nomiclabs/hardhat-etherscan");
  * @type import('hardhat/config').HardhatUserConfig
  */
 const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY;
+
+console.log(process.env);
+
 module.exports = {
   solidity: {
+
     compilers: [
       {
         version: "0.8.9",
@@ -19,24 +23,46 @@ module.exports = {
       { version: "0.6.8", settings: {} },
       { version: "0.7.4", settings: {} },
     ],
+
   },
+
   paths: {
     artifacts: "./packages/frontend/src/artifacts"
   },
+
   namedAccounts: {
     deployer: 0,
   },
+
   networks: {
+
+    defaultNetwork: "hardhat",
+
+    hardhat: {
+      forking: {
+        url: "https://speedy-nodes-nyc.moralis.io/63842bcca9982a74b2a9fd41/bsc/mainnet",
+      },
+      accounts: [
+        {
+          privateKey: `0x${ACCOUNT_PRIVATE_KEY}`,
+          balance: "1000000000000000000000"
+        }
+      ],
+    },
+
     mainnet: {
-      url: process.env.MAINNET_HTTP_URL,
+      url: process.env.HTTP_URL,
       accounts: [`0x${ACCOUNT_PRIVATE_KEY}`],
     },
+
     testnet: {
-      url: process.env.TESTNET_HTTP_URL,
+      url: process.env.HTTP_URL,
       accounts: [`0x${ACCOUNT_PRIVATE_KEY}`],
     }
   },
+
   etherscan: {
     apiKey: "X2WHYR6AHQNZAUN9HX9J65CGVNN15JPZD2"
   }
+
 };
